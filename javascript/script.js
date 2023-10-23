d55fa92789d3d49dcdd4b413b83eb19e33b34398
Marco Rampin <rampin.marco.17@gmail.com>
2023-10-23 11:46:00 +0200

    Added navigation bar and animation

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	$("nav a").on('click',function(){
+   	var link = $(this).attr('href');
+   	$('html,body').animate({scrollTop: ($(link).offset().top - 100)},'slow');
+   	return false;
+	});
+
     const resizeHeight = () => {
 		$('html').css('--maxHeight', window.innerHeight+'px');
 	}
@@ -8,6 +14,19 @@ $(document).ready(function() {
 		resizeHeight();
 	}
 
-    window.addEventListener("resize", resizeHeight);
+   window.addEventListener("resize", resizeHeight);
   	window.addEventListener('load', loadFunctions());
 });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon 
+See online if it's possible to add a Event listener to see the click*/
+/*
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+*/
